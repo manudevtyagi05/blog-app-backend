a5da4001078b6c1ef5b34f2b6ec1afe389f72461
manudevtyagi05 <137281508+manudevtyagi05@users.noreply.github.com>
2025-07-17 22:30:33 +0530

    Added UserRepository interface

--- src/main/java/com/blogapp/repository/UserRepository.java
+++ src/main/java/com/blogapp/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.blogapp.repository;
 
-public class UserRepository {
+import com.blogapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // Custom finder method
+    User findByEmail(String email);
 }
